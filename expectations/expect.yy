6febd8a72b475d5367821edca73539b874d875dd
reynolds <reynolds@192cc142-4ac5-4de2-ad3d-7c493c50dd98>
2005-02-19 02:49:10 +0000

    Name change when we went to C++.  Lots of overhauled stuff in both files.
    In particular: new mode to print out tree in (almost) syntatically valid
    expectation format.
    
    
    git-svn-id: svn+ssh://aws/home/ubuntu/SVN/trunk/pip@1124 192cc142-4ac5-4de2-ad3d-7c493c50dd98

---
+++ expectations/expect.yy
@@ -0,0 +1,257 @@
+%{
+#include "parsetree.h"
+%}
+%union {
+	int iValue;
+	char *sValue;
+	Symbol *symbol;
+	Node *nPtr;
+};
+
+// keywords
+%token EXPECTATION
+%token FRAGMENT
+%token REPEAT
+%token REVERSE
+%token JOIN
+%token BRANCH
+%token SPLIT
+%token XOR
+%token CALL
+%token BETWEEN
+%token AND
+%token ASSERT
+%token INSTANCES
+%token UNIQUE
+%token DURING
+%token ANY
+%token AVERAGE
+%token STDDEV
+%token F_MAX
+// operators
+%token GE
+%token LE
+%token EQ
+%token NE
+%token B_AND
+%token B_OR
+%token IMPLIES
+%token IN
+%token LIST
+// operations
+%token MESSAGE
+%token TASK
+%token EVENT
+%token LIMIT
+// tokens
+%token ELLIPSIS
+%token RANGE
+%token <sValue> STRING
+%token <sValue> REGEX
+%token <iValue> INTEGER
+%token <symbol> IDENTIFIER
+%token <symbol> STRINGVAR
+%token <symbol> PATHVAR
+%type <nPtr> pathdecl ident_list statement statement_list thread_list thread
+%type <nPtr> thread_set repeat limit_list limit range repeat_range path_expr
+%type <nPtr> string_expr event xor_list xor task assert assertdecl bool_expr
+%type <nPtr> int_expr window float_expr string_literal
+
+%left B_AND B_OR IMPLIES
+%left '<' '>' LE GE EQ NE
+%left '+' '-'
+%left '*' '/'
+%nonassoc '!'
+%{
+#include <stdarg.h>
+#include <stdio.h>
+void yyerror(const char *fmt, ...);
+int yylex(void);
+extern int yylno;
+extern FILE *yyin;
+char *filename;
+%}
+
+%%
+
+program:
+		program pathdecl													{ print_tree($2, 0); }
+		| program assertdecl											{ print_tree($2, 0); }
+		|
+		;
+
+pathdecl:
+		EXPECTATION IDENTIFIER '{' statement_list '}'	{ $$ = opr(EXPECTATION, 2, id($2), $4); }
+		| FRAGMENT IDENTIFIER '{' statement_list '}'	{ $$ = opr(FRAGMENT, 2, id($2), $4); }
+		;
+
+ident_list:
+		ident_list ',' IDENTIFIER									{ $$ = opr(',', 2, $1, id($3)); }
+		| IDENTIFIER															{ $$ = id($1); }
+		;
+
+statement_list:
+		statement_list statement									{ $$ = opr(';', 2, $1, $2); }
+		|																					{ $$ = NULL; }
+		;
+		
+statement:
+		REVERSE '(' path_expr ')' ';'							{ $$ = opr(REVERSE, 1, $3); }
+		| MESSAGE '(' ')' ';'											{ $$ = opr(MESSAGE, 0); }
+		| CALL '(' IDENTIFIER ')' ';'							{ $$ = opr(CALL, 1, id($3)); }
+		| event ';'
+		| task limit_list ';'											{ $$ = opr(TASK, 3, $1, $2, NULL); }
+		| task limit_list '{' statement_list '}'	{ $$ = opr(TASK, 3, $1, $2, $4); }
+		| string_expr
+		| path_expr
+		| SPLIT '{' thread_list '}' JOIN '(' thread_set ')' ';'		{ $$ = opr(SPLIT, 2, $3, $7); }
+		| XOR '{' xor_list '}'										{ $$ = opr(XOR, 1, $3); }
+		| '{' statement_list '}'									{ $$ = $2; }
+		| error ';'																{ $$ = NULL; }
+		;
+
+xor_list:
+		xor_list xor															{ $$ = opr(LIST, 2, $1, $2); }
+		|																					{ $$ = NULL; }
+		;
+
+xor:
+		BRANCH ':' statement_list									{	$$ = opr(BRANCH, 1, $3); }
+		;
+
+thread_list:
+		thread_list thread												{ $$ = opr(LIST, 2, $1, $2); }
+		|																					{ $$ = NULL; }
+		;
+
+thread:
+		BRANCH IDENTIFIER ':' statement_list			{	$$ = opr(BRANCH, 2, id($2), $4); }
+		;
+
+thread_set:
+		ident_list
+		| INTEGER																	{ $$ = new IntNode($1); }
+		;
+
+repeat:
+		REPEAT repeat_range statement							{ $$ = opr(REPEAT, 2, $2, $3); }
+		;
+
+limit_list:
+		limit_list limit													{ $$ = opr(LIST, 2, $1, $2); }
+		|																					{ $$ = NULL; }
+		;
+
+limit:
+		LIMIT '(' IDENTIFIER ',' range ')'				{ $$ = opr(LIMIT, 2, id($3), $5); }
+		;
+
+range:
+		'{' float_expr ELLIPSIS float_expr '}'		{ $$ = opr(RANGE, 2, $2, $4); }
+		| '{' float_expr ELLIPSIS '}'							{ $$ = opr(RANGE, 2, $2, NULL); }
+		;
+
+repeat_range:
+		INTEGER																		{ $$ = opr(RANGE, 2, new IntNode($1), new IntNode($1)); }
+		| BETWEEN INTEGER AND INTEGER							{ $$ = opr(RANGE, 2, new IntNode($2), new IntNode($4)); }
+		;
+
+path_expr:
+		repeat
+		| PATHVAR																	{ $$ = id($1); }
+		| PATHVAR '=' path_expr										{ $$ = opr('=', 2, id($1), $3); }
+		;
+
+task:
+		TASK '(' string_expr ',' string_expr ')'			{ $$ = opr(TASK, 2, $3, $5); }
+		;
+
+event:
+		EVENT '(' string_expr ',' string_expr ')'			{ $$ = opr(EVENT, 2, $3, $5); }
+		;
+
+string_literal:
+		STRING																		{ $$ = new StringNode(false, $1); }
+		| REGEX																		{ $$ = new StringNode(true, $1); }
+		;
+
+string_expr:
+		string_literal
+		| STRINGVAR																{ $$ = id($1); }
+		| STRINGVAR '=' string_expr								{ $$ = opr('=', 2, id($1), $3); }
+		;
+
+assertdecl:
+		DURING '(' window ')' assert							{ $$ = opr(DURING, 2, $3, $5); }
+		| assert
+		;
+
+window:
+		INTEGER ',' INTEGER												{ $$ = opr(RANGE, 2, new IntNode($1), new IntNode($3)); }
+		| ANY INTEGER															{ $$ = opr(ANY, 1, new IntNode($2)); }
+		;
+
+assert:
+		ASSERT '(' bool_expr ')'									{ $$ = opr(ASSERT, 1, $3); }
+		;
+
+bool_expr:
+		bool_expr B_AND bool_expr									{ $$ = opr(B_AND, 2, $1, $3); }
+		| bool_expr B_OR bool_expr								{ $$ = opr(B_OR, 2, $1, $3); }
+		| bool_expr IMPLIES bool_expr							{ $$ = opr(IMPLIES, 2, $1, $3); }
+		| '!' bool_expr														{ $$ = opr('!', 1, $2); }
+		| '(' bool_expr ')'												{ $$ = $2; }
+		| float_expr IN range											{ $$ = opr(IN, 2, $1, $3); }
+		| float_expr '<' float_expr								{ $$ = opr('<', 2, $1, $3); }
+		| float_expr '>' float_expr								{ $$ = opr('>', 2, $1, $3); }
+		| float_expr LE float_expr								{ $$ = opr(LE, 2, $1, $3); }
+		| float_expr GE float_expr								{ $$ = opr(GE, 2, $1, $3); }
+		| float_expr EQ float_expr								{ $$ = opr(EQ, 2, $1, $3); }
+		| float_expr NE float_expr								{ $$ = opr(NE, 2, $1, $3); }
+		;
+
+float_expr:
+		int_expr
+		| AVERAGE '(' IDENTIFIER ',' IDENTIFIER ')'		{ $$ = opr(AVERAGE, 2, id($3), id($5)); }
+		| AVERAGE '(' IDENTIFIER ',' string_literal ')'		{ $$ = opr(AVERAGE, 2, id($3), $5); }
+		| STDDEV '(' IDENTIFIER ',' IDENTIFIER ')'		{ $$ = opr(STDDEV, 2, id($3), id($5)); }
+		| STDDEV '(' IDENTIFIER ',' string_literal ')'		{ $$ = opr(STDDEV, 2, id($3), $5); }
+		| F_MAX '(' IDENTIFIER ',' IDENTIFIER ')'				{ $$ = opr(F_MAX, 2, id($3), id($5)); }
+		| F_MAX '(' IDENTIFIER ',' string_literal ')'				{ $$ = opr(F_MAX, 2, id($3), $5); }
+		;
+
+int_expr:
+		INTEGER																		{ $$ = new IntNode($1); }
+		| INSTANCES '(' IDENTIFIER ')'						{ $$ = opr(INSTANCES, 1, id($3)); }
+		| UNIQUE '(' IDENTIFIER ')'								{ $$ = opr(UNIQUE, 1, id($3)); }
+		| UNIQUE '(' STRINGVAR ')'								{ $$ = opr(UNIQUE, 1, id($3)); }
+		;
+
+%%
+
+void yyerror(const char *fmt, ...) {
+	va_list args;
+	fprintf(stderr, "%s:%d: ", filename, yylno);
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fputc('\n', stderr);
+}
+
+int main(int argc, char **argv) {
+	if (argc > 1) {
+		filename = argv[1];
+		yyin = fopen(filename, "r");
+		if (!yyin) {
+			perror(filename);
+			return 1;
+		}
+		yyparse();
+		fclose(yyin);
+	}
+	else {
+		filename = "<stdin>";
+		yyparse();
+	}
+	return 0;
+}
---
+++ expectations/parsetree.cc
@@ -0,0 +1,395 @@
+#include <map>
+#include <string>
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "parsetree.h"
+#include "expect.tab.hh"
+
+static std::map<std::string, Symbol*> symbol_table;
+
+Symbol *symbol_lookup(const char *name) {
+	Symbol *sym = symbol_table[name];
+	if (!sym) {
+		sym = new Symbol(name);
+		symbol_table[name] = sym;
+	}
+	return sym;
+}
+
+OperatorNode::OperatorNode(int which, int argc, ...) {
+	op = which;
+	noperands = argc;
+	va_list args;
+	va_start(args, argc);
+	for (int i=0; i<argc; i++)
+		operands.push_back(va_arg(args, Node*));
+	va_end(args);
+}
+
+OperatorNode::~OperatorNode(void) {
+}
+
+struct {
+	int op;
+	const char *name;
+} opmap[] = {
+	{ EXPECTATION, "EXPECTATION" },
+	{ FRAGMENT, "FRAGMENT" },
+	{ REPEAT, "REPEAT" },
+	{ REVERSE, "REVERSE" },
+	{ JOIN, "JOIN" },
+	{ BRANCH, "BRANCH" },
+	{ SPLIT, "SPLIT" },
+	{ XOR, "XOR" },
+	{ CALL, "CALL" },
+	{ BETWEEN, "BETWEEN" },
+	{ AND, "AND" },
+	{ ASSERT, "ASSERT" },
+	{ INSTANCES, "INSTANCES" },
+	{ UNIQUE, "UNIQUE" },
+	{ DURING, "DURING" },
+	{ ANY, "ANY" },
+	{ AVERAGE, "AVERAGE" },
+	{ STDDEV, "STDDEV" },
+	{ F_MAX, "F_MAX" },
+	{ GE, "GE" },
+	{ LE, "LE" },
+	{ EQ, "EQ" },
+	{ NE, "NE" },
+	{ B_AND, "B_AND" },
+	{ B_OR, "B_OR" },
+	{ IMPLIES, "IMPLIES" },
+	{ IN, "IN" },
+	{ MESSAGE, "MESSAGE" },
+	{ TASK, "TASK" },
+	{ EVENT, "EVENT" },
+	{ LIMIT, "LIMIT" },
+	{ ELLIPSIS, "ELLIPSIS" },
+	{ RANGE, "RANGE" },
+	{ STRING, "STRING" },
+	{ REGEX, "REGEX" },
+	{ INTEGER, "INTEGER" },
+	{ IDENTIFIER, "IDENTIFIER" },
+	{ STRINGVAR, "STRINGVAR" },
+	{ PATHVAR, "PATHVAR" },
+	{ -1, "" },
+};
+
+#if 1
+/* ALMOST print out the input.
+ * 1)   The "RANGE" operator is used in three different contexts:
+ *       - repeat between 1 and 5 { ... }
+ *       - during(1, 5) ...
+ *       - limit(CPU_TIME, {1...5})
+ *      We print it out as "{1...5}" and so it does not parse right in the
+ *      "repeat" or "during" contexts.
+ * 2)   Blank lines, spacing, and comments.  Duh.
+ * 3)   Parentheses get lost.
+ * 4)   "repeat" statements always get {} around their body
+ * 5)   Indentation weirdness with @PATH=repeat
+ */
+#define TAB printf("%*s", 2*depth, "");
+void print_tree(const Node *node, int depth) {
+	if (!node) { return; }
+	switch (node->type()) {
+		case NODE_INT:
+			printf("%d", ((IntNode*)node)->value);
+			break;
+		case NODE_STRING:
+			printf("\"%s\"", ((StringNode*)node)->s);
+			break;
+		case NODE_REGEX:
+			printf("m/%s/", ((StringNode*)node)->s);
+			break;
+		case NODE_IDENTIFIER:
+			printf("%s", ((IdentifierNode*)node)->sym->name.c_str());
+			break;
+		case NODE_OPERATOR:{
+			OperatorNode *onode = (OperatorNode*)node;
+			switch (onode->op) {
+				case EXPECTATION:
+					TAB printf("expectation ");
+					print_tree(onode->operands[0], depth);
+					printf(" {\n");
+					print_tree(onode->operands[1], depth+1);
+					TAB printf("}\n");
+					break;
+				case FRAGMENT:
+					TAB printf("fragment ");
+					print_tree(onode->operands[0], depth);
+					printf(" {\n");
+					print_tree(onode->operands[1], depth+1);
+					TAB printf("}\n");
+					break;
+				case ASSERT:
+					TAB printf("assert( ");
+					print_tree(onode->operands[0], depth);
+					printf(" )\n");
+					break;
+				case LIST:
+					print_tree(onode->operands[0], depth);
+					print_tree(onode->operands[1], depth);
+					break;
+				case ',':
+					print_tree(onode->operands[0], depth);
+					printf(", ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case ';':
+					print_tree(onode->operands[0], depth);
+					print_tree(onode->operands[1], depth);
+					break;
+				case REVERSE:
+					TAB printf("reverse(");
+					print_tree(onode->operands[0], depth);
+					printf(");\n");
+					break;
+				case MESSAGE:
+					TAB printf("message();\n");
+					break;
+				case CALL:
+					TAB printf("call(");
+					print_tree(onode->operands[0], depth);
+					printf(");\n");
+					break;
+				case TASK:
+					switch (onode->noperands) {
+						case 2:
+							TAB printf("task(");
+							print_tree(onode->operands[0], depth);
+							printf(", ");
+							print_tree(onode->operands[1], depth);
+							printf(")");
+							break;
+						case 3:
+							print_tree(onode->operands[0], depth);
+							print_tree(onode->operands[1], depth+1);
+							if (onode->operands[2]) {
+								printf(" {\n");
+								print_tree(onode->operands[2], depth+1);
+								TAB printf("}\n");
+							}
+							else
+								printf(";\n");
+							break;
+						default:
+							assert(!"invalid number of operands for TASK");
+					}
+					break;
+				case EVENT:
+					TAB printf("event(");
+					print_tree(onode->operands[0], depth);
+					printf(", ");
+					print_tree(onode->operands[1], depth);
+					printf(");\n");
+					break;
+				case XOR:
+					TAB printf("xor {\n");
+					print_tree(onode->operands[0], depth+1);
+					TAB printf("}\n");
+					break;
+				case LIMIT:
+					printf("\n");
+					TAB printf("limit(");
+					print_tree(onode->operands[0], depth);
+					printf(", ");
+					print_tree(onode->operands[1], depth);
+					printf(")");
+					break;
+				case RANGE:
+					printf("{");
+					print_tree(onode->operands[0], depth);
+					printf("...");
+					if (onode->operands[1]) print_tree(onode->operands[1], depth);
+					printf("}");
+					break;
+				case BRANCH:
+					switch (onode->noperands) {
+						case 1:
+							TAB printf("branch:\n");
+							print_tree(onode->operands[0], depth+1);
+							break;
+						case 2:
+							TAB printf("branch ");
+							print_tree(onode->operands[0], depth);
+							printf(":\n");
+							print_tree(onode->operands[1], depth+1);
+							break;
+						default:
+							assert(!"invalid number of operands for BRANCH");
+					}
+					break;
+				case SPLIT:
+					TAB printf("split {\n");
+					print_tree(onode->operands[0], depth+1);
+					TAB printf("} join (");
+					print_tree(onode->operands[1], depth+1);
+					printf(");\n");
+					break;
+				case REPEAT:
+					TAB printf("repeat ");
+					print_tree(onode->operands[0], depth);
+					printf(" {\n");
+					print_tree(onode->operands[1], depth+1);
+					TAB printf("}\n");
+					break;
+				case '=':
+					print_tree(onode->operands[0], depth);
+					printf("%c", onode->op);
+					print_tree(onode->operands[1], depth);
+					break;
+				case '<':
+				case '>':
+					print_tree(onode->operands[0], depth);
+					printf(" %c ", onode->op);
+					print_tree(onode->operands[1], depth);
+					break;
+				case DURING:
+					TAB printf("during(");
+					print_tree(onode->operands[0], depth);
+					printf(") ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case ANY:
+					printf("any ");
+					print_tree(onode->operands[0], depth);
+					break;
+				case B_AND:
+					print_tree(onode->operands[0], depth);
+					printf(" && ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case B_OR:
+					print_tree(onode->operands[0], depth);
+					printf(" || ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case IMPLIES:
+					print_tree(onode->operands[0], depth);
+					printf(" -> ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case IN:
+					print_tree(onode->operands[0], depth);
+					printf(" in ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case LE:
+					print_tree(onode->operands[0], depth);
+					printf(" <= ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case GE:
+					print_tree(onode->operands[0], depth);
+					printf(" >= ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case EQ:
+					print_tree(onode->operands[0], depth);
+					printf(" == ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case NE:
+					print_tree(onode->operands[0], depth);
+					printf(" != ");
+					print_tree(onode->operands[1], depth);
+					break;
+				case '!':
+					printf("!");
+					print_tree(onode->operands[0], depth);
+					break;
+				case INSTANCES:
+					printf("instances(");
+					print_tree(onode->operands[0], depth);
+					printf(")");
+					break;
+				case UNIQUE:
+					printf("unique(");
+					print_tree(onode->operands[0], depth);
+					printf(")");
+					break;
+				case F_MAX:
+					printf("max(");
+					print_tree(onode->operands[0], depth);
+					printf(", ");
+					print_tree(onode->operands[1], depth);
+					printf(")");
+					break;
+				case AVERAGE:
+					printf("average(");
+					print_tree(onode->operands[0], depth);
+					printf(", ");
+					print_tree(onode->operands[1], depth);
+					printf(")");
+					break;
+				case STDDEV:
+					printf("stddev(");
+					print_tree(onode->operands[0], depth);
+					printf(", ");
+					print_tree(onode->operands[1], depth);
+					printf(")");
+					break;
+				default:
+					printf("\n\n\nunhandled operator: ");
+					if (onode->op <= 255)
+						printf("%d (%c)\n", onode->op, onode->op);
+					else {
+						int found = 0;
+						for (int i=0; opmap[i].op > 0; i++)
+							if (onode->op == opmap[i].op) {
+								printf("%d (%s)\n", onode->op, opmap[i].name);
+								found = 1;
+								break;
+							}
+						if (!found)
+							printf("%d (!UNKNOWN!)\n", onode->op);
+					}
+					exit(1);
+			}
+			break;}
+		default:
+			assert(!"not reached");
+	}
+}
+#else
+void print_tree(const Node *node, int depth) {
+	int i;
+	printf("%*sNode %p: ", depth*2, "", node);
+	if (!node) { printf("NULL\n"); return; }
+	switch (node->type()) {
+		case NODE_INT:
+			printf("int: %d\n", ((IntNode*)node)->value);
+			break;
+		case NODE_STRING:
+			printf("string: \"%s\"\n", ((StringNode*)node)->s);
+			break;
+		case NODE_REGEX:
+			printf("regex: \"%s\"\n", ((StringNode*)node)->s);
+			break;
+		case NODE_IDENTIFIER:
+			printf("identifier: \"%s\"\n", ((IdentifierNode*)node)->sym->name.c_str());
+			break;
+		case NODE_OPERATOR:{
+			OperatorNode *onode = (OperatorNode*)node;
+			if (onode->op <= 255)
+				printf("operator %d (%c)\n", onode->op, onode->op);
+			else {
+				int found = 0;
+				for (i=0; opmap[i].op > 0; i++)
+					if (onode->op == opmap[i].op) {
+						printf("operator %d (%s)\n", onode->op, opmap[i].name);
+						found = 1;
+						break;
+					}
+				if (!found)
+					printf("operator %d (!UNKNOWN!)\n", onode->op);
+			}
+			for (i=0; i<onode->noperands; i++)
+				print_tree(onode->operands[i], depth+1);
+			break;}
+		default:
+			assert(!"not reached");
+	}
+}
+#endif
